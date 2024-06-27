29c9ac74bc912c78769ff369e069bc0b257097bf
Forrest Crawford <FCrawford1992@gmail.com>
2024-06-27 00:10:08 -0400

    Adding bitmap cache.

---
+++ Direct2DDXFViewer/BitmapCache.cs
@@ -0,0 +1,16 @@
+﻿using SharpDX.Direct2D1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Direct2DDXFViewer
+{
+    public class BitmapCache
+    {
+        public Dictionary<float, Bitmap1> Bitmaps { get; set; } = new();
+        public float currentZoom = 1.0f;
+        public int currentWidth = 0;
+    }
+}
