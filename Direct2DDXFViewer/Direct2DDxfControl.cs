34c37bfdeca44a8b9d8be52b6c5572f74c49d028
Forrest Crawford <FCrawford1992@gmail.com>
2024-07-22 20:20:39 -0400

    Nope.

--- Direct2DDXFViewer/Direct2DDxfControl.cs
+++ Direct2DDXFViewer/Direct2DDxfControl.cs
@@ -383,7 +383,7 @@ namespace Direct2DDXFViewer
                 RawRectangleF destRect = new(0, 0, (float)ActualWidth, (float)ActualHeight);
                 RawRectangleF sourceRect = new((float)node.Bounds.Left, (float)node.Bounds.Top, (float)node.Bounds.Right, (float)node.Bounds.Bottom);
 
-                //Debug.WriteLine($"sourceRect: {sourceRect.Left} {sourceRect.Top} {sourceRect.Right} {sourceRect.Bottom}");
+                Debug.WriteLine($"\n\n\nsourceRect: {sourceRect.Left} {sourceRect.Top} {sourceRect.Right} {sourceRect.Bottom}");
 
                 target.DrawBitmap(node.Bitmap, destRect, 1.0f, BitmapInterpolationMode.Linear, sourceRect);
             }
