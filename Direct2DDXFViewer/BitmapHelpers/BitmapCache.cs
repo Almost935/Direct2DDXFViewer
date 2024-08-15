581a6092a8f3b5a2413c4736d8f50b1f855c18d9
Forrest Crawford <FCrawford1992@gmail.com>
2024-08-14 21:50:52 -0400

    8.14.24

--- Direct2DDXFViewer/BitmapHelpers/BitmapCache.cs
+++ Direct2DDXFViewer/BitmapHelpers/BitmapCache.cs
@@ -1,22 +1,140 @@
-﻿using System;
+﻿using Direct2DDXFViewer.DrawingObjects;
+using SharpDX;
+using SharpDX.Direct2D1;
+using SharpDX.Mathematics.Interop;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace Direct2DDXFViewer.BitmapHelpers
 {
-    public class BitmapCache
+    public class BitmapCache : INotifyPropertyChanged
     {
         #region Fields
         private Dictionary<float, DxfBitmap> _bitmaps = new();
+        private DeviceContext1 _deviceContext;
+        private ObjectLayerManager _layerManager;
+        private float _currentZoom;
+        private RawMatrix3x2 _extentsMatrix;
+        private float _zoomFactor;
+
+        private const int initializationFactor = 5;
         #endregion
 
         #region Properties
+        public float CurrentZoom
+        {
+            get => _currentZoom;
+            set
+            {
+                if (_currentZoom != value)
+                {
+                    _currentZoom = value;
+                    OnPropertyChanged(nameof(CurrentZoom));
+                }
+            }
+        }
+        #endregion
+
+        #region
+        public BitmapCache(DeviceContext1 deviceContext, ObjectLayerManager layerManager, RawMatrix3x2 extentsMatrix, float zoomFactor)
+        {
+            _deviceContext = deviceContext;
+            _layerManager = layerManager;
+            _extentsMatrix = extentsMatrix;
+            _zoomFactor = zoomFactor;
+
+            InitializeBitmaps();    
+        }
         #endregion
 
         #region Methods
-        public GetDxfBitmap
+        public DxfBitmap GetDxfBitmap(float zoom)
+        {
+            _bitmaps.TryGetValue(zoom, out DxfBitmap bitmap);
+            return bitmap;
+        }
+
+        public void InitializeBitmaps()
+        {
+            if (_layerManager is not null && _deviceContext is not null)
+            {
+                // Initialize first view
+                _bitmaps.TryAdd((float)Math.Round((double)1, 3), new DxfBitmap(_deviceContext, 1, RenderBitmap(_deviceContext, 1)));
+
+                // Initialize zoomed in bitmaps
+                float zoom = 1;
+                for (int i = 1; i <= initializationFactor; i++)
+                {
+                    zoom *= _zoomFactor;
+                    _bitmaps.TryAdd((float)Math.Round((double)zoom, 3), new DxfBitmap(_deviceContext, zoom, RenderBitmap(_deviceContext, zoom)));
+                }
+
+                // Initialize zoomed out bitmaps
+                zoom = 1;
+                for (int i = 1; i <= initializationFactor; i++)
+                {
+                    zoom *= (1 / _zoomFactor);
+                    _bitmaps.TryAdd((float)Math.Round((double)zoom, 3), new DxfBitmap(_deviceContext, zoom, RenderBitmap(_deviceContext, zoom)));
+                }
+            }
+        }
+
+        private void UpdateBitmapsDictionary(float intialZoom)
+        {
+            if (_layerManager is not null && _deviceContext is not null)
+            {
+                // Initialize first view
+                _bitmaps.TryAdd((float)Math.Round((double)1, 3), new DxfBitmap(_deviceContext, 1, RenderBitmap(_deviceContext, 1)));
+
+                // Initialize zoomed in bitmaps
+                float zoom = 1;
+                for (int i = 1; i <= initializationFactor; i++)
+                {
+                    zoom *= _zoomFactor;
+
+                    _bitmaps.TryAdd((float)Math.Round((double)zoom, 3), new DxfBitmap(_deviceContext, zoom, RenderBitmap(_deviceContext, zoom)));
+                }
+
+                // Initialize zoomed out bitmaps
+                zoom = 1;
+                for (int i = 1; i <= initializationFactor; i++)
+                {
+                    zoom *= (1 / _zoomFactor);
+                    _bitmaps.TryAdd((float)Math.Round((double)zoom, 3), new DxfBitmap(_deviceContext, zoom, RenderBitmap(_deviceContext, zoom)));
+                }
+            }
+        }
+
+        private Bitmap RenderBitmap(DeviceContext1 deviceContext, float zoom)
+        {
+            Size2F size = new(deviceContext.Size.Width * zoom, deviceContext.Size.Height * zoom);
+            BitmapRenderTarget bitmapRenderTarget = new(deviceContext, CompatibleRenderTargetOptions.None, size)
+            {
+                DotsPerInch = new Size2F(96.0f * zoom, 96.0f * zoom),
+                AntialiasMode = AntialiasMode.Aliased
+            };
+
+            bitmapRenderTarget.BeginDraw();
+            bitmapRenderTarget.Transform = _extentsMatrix;
+            _layerManager.DrawToRenderTarget(bitmapRenderTarget, 1);
+            bitmapRenderTarget.EndDraw();
+            return bitmapRenderTarget.Bitmap;
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Events
+        public event PropertyChangedEventHandler PropertyChanged;
         #endregion
     }
 }
--- Direct2DDXFViewer/BitmapHelpers/DxfBitmap.cs
+++ Direct2DDXFViewer/BitmapHelpers/DxfBitmap.cs
@@ -1,34 +1,98 @@
-﻿using SharpDX.Direct2D1;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
+using System.IO;
+using SharpDX;
+using SharpDX.Direct2D1;
+using SharpDX.WIC;
+
+using Bitmap = SharpDX.Direct2D1.Bitmap;
 
 namespace Direct2DDXFViewer.BitmapHelpers
 {
     public class DxfBitmap
     {
         #region Fields
-        private string _folderpath;
+        private DeviceContext1 _deviceContext;
         private string _filepath;
         #endregion
 
         #region Properties
         public float Zoom { get; set; }
         public Bitmap Bitmap { get; set; }
+        public bool BitmapLoaded => Bitmap != null;
+        public bool BitmapSaved = false;
         #endregion
 
         #region Constructor
-        public DxfBitmap(float zoom, string filepath, Bitmap bitmap)
+        public DxfBitmap(DeviceContext1 deviceContext, float zoom, Bitmap bitmap)
         {
+            _deviceContext = deviceContext;
             Zoom = zoom;
             Bitmap = bitmap;
-            _filepath = filepath;
         }
         #endregion
 
         #region Methods
+        public void LoadBitmapFromFile(string filePath)
+        {
+            if (BitmapSaved)
+            {
+                ImagingFactory imagingFactory = new();
+                // Load the image using WIC
+                using (var decoder = new BitmapDecoder(imagingFactory, filePath, DecodeOptions.CacheOnLoad))
+                using (var frame = decoder.GetFrame(0))
+                using (var converter = new FormatConverter(imagingFactory))
+                {
+                    // Convert the image format to 32bppPBGRA which is compatible with Direct2D
+                    converter.Initialize(frame, SharpDX.WIC.PixelFormat.Format32bppPBGRA);
+
+                    // Create a Direct2D Bitmap from the WIC Bitmap
+                    Bitmap = Bitmap.FromWicBitmap(_deviceContext, converter);
+                }
+                imagingFactory.Dispose();
+            }
+        }
+
+
+        public void SaveBitmapToTemporaryFile()
+        {
+            if (!BitmapSaved)
+            {
+                ImagingFactory imagingFactory = new();
+                // Create a WIC Bitmap
+                var wicBitmap = new SharpDX.WIC.Bitmap(imagingFactory, Bitmap.PixelSize.Width, Bitmap.PixelSize.Height,
+                                            SharpDX.WIC.PixelFormat.Format32bppPBGRA, BitmapCreateCacheOption.CacheOnLoad);
+
+                // Create a RenderTarget for the WIC Bitmap
+                using (var renderTarget = new WicRenderTarget(Bitmap.Factory, wicBitmap, new RenderTargetProperties()))
+                {
+                    // Copy the Direct2D Bitmap to the WIC Bitmap
+                    renderTarget.BeginDraw();
+                    renderTarget.DrawBitmap(Bitmap, 1.0f, SharpDX.Direct2D1.BitmapInterpolationMode.NearestNeighbor);
+                    renderTarget.EndDraw();
+                }
+
+                _filepath = Path.Combine(Path.GetTempPath(), $"{Guid.NewGuid()}.png");
+
+                // Select encoder based on file extension
+                BitmapEncoder encoder;
+                using (var stream = new WICStream(imagingFactory, _filepath, SharpDX.IO.NativeFileAccess.Write))
+                {
+                    encoder = new PngBitmapEncoder(imagingFactory, stream);
+                    var frame = new BitmapFrameEncode(encoder);
+                    frame.Initialize();
+                    frame.SetSize(wicBitmap.Size.Width, wicBitmap.Size.Height);
+                    var guid = SharpDX.WIC.PixelFormat.Format32bppPBGRA;
+                    frame.SetPixelFormat(ref guid);
+                    frame.WriteSource(wicBitmap);
+                    frame.Commit();
+                    encoder.Commit();
+                }
+                wicBitmap.Dispose();
+                imagingFactory.Dispose();
+
+                BitmapSaved = true;
+            }
+        }
         #endregion
     }
 }
